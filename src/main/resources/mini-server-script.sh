b3ebe587076173b1e37f15cfc9a6a008bac2d911
Evan Hallmark <ehallmark@gttgrp.com>
2017-08-02 10:02:50 -0700

    Chagnse

---
+++ src/main/resources/mini-server-script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd /home/ehallmark1122/PlatformStarter
+sudo -u ehallmark1122 bash -c '    echo "Changed Dirs"'
+sudo -u ehallmark1122 bash -c '    git pull origin master'
+sudo -u ehallmark1122 bash -c '    echo "Pulled from git"'
+sudo -u ehallmark1122 bash -c '    /opt/maven/bin/mvn clean install'
+sudo -u ehallmark1122 bash -c '    echo "Mvn clean installed"'
+sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" main.java.mini_server.Server &'
