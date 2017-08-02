e05204a7f915b3d6ffa75f43bb36847f3a6b8a07
Evan Hallmark <ehallmark@gttgrp.com>
2017-08-02 16:02:53 -0700

    small change

--- src/main/java/mini_server/StartUpTask.java
+++ src/main/java/mini_server/StartUpTask.java
@@ -1,5 +1,7 @@
 package mini_server;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.TimerTask;
 
 /**
@@ -8,6 +10,11 @@ import java.util.TimerTask;
 public class StartUpTask extends TimerTask {
     @Override
     public void run() {
+        if(LocalDate.now().getDayOfWeek().equals(DayOfWeek.SUNDAY) || LocalDate.now().getDayOfWeek().equals(DayOfWeek.SATURDAY)) {
+            System.out.println("Not running on the weekend...");
+            return;
+        }
+        // Weekday
         System.out.println("Server should start soon...");
         Server.keepOn.set(true);
     }
