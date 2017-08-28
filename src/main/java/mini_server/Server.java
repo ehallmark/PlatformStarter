c9e4614a98ae00aa465cdeb3d7aae7419309cc9d
Evan Hallmark <ehallmark@gttgrp.com>
2017-08-28 10:33:41 -0700

    c

--- src/main/java/mini_server/Server.java
+++ src/main/java/mini_server/Server.java
@@ -3,6 +3,7 @@ package mini_server;
 import java.time.*;
 import java.util.Timer;
 import java.util.concurrent.atomic.AtomicBoolean;
+import static spark.Spark.*;
 
 /**
  * Created by ehallmark on 8/1/17.
@@ -11,12 +12,20 @@ public class Server {
     private static final String DEFAULT_URL = "http://35.196.70.117";
     private static final String DEFAULT_ZONE = "us-east1-c";
     private static final String DEFAULT_INSTANCE_NAME = "instance-5";
-    public static void main(String[] args) {
-        Timer timer = new Timer();
-        final long monitorPeriod = 10 * 60 * 1000; // a few minutes
+
+    private static final MonitorTask monitorTask;
+    private static final Timer timer;
+    static {
+        timer = new Timer();
         // monitor
-        MonitorTask monitorTask = new MonitorTask(DEFAULT_URL, DEFAULT_INSTANCE_NAME, DEFAULT_ZONE);
-        timer.schedule(monitorTask,0,monitorPeriod);
+        monitorTask = new MonitorTask(DEFAULT_URL, DEFAULT_INSTANCE_NAME, DEFAULT_ZONE);
+    }
+    public static void main(String[] args) {
+        final long monitorPeriod = 10 * 60 * 1000; // a few minutes
+        //timer.schedule(monitorTask,0,monitorPeriod);
+
+        server();
+
     }
 
 
@@ -41,4 +50,11 @@ public class Server {
         return true;
     }
 
+    private static void server() {
+        port(8080);
+
+        get("/", (req,res)->{
+           return "Hello World!";
+        });
+    }
 }
